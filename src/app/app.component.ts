49deb99220c8a915a2791214afadaa058507fe4b
jeffkrantz <jjkrantz@gmail.com>
2017-07-17 22:56:52 -0500

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+export class Hero{
+  id: number;
+  name: string;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  heroes:Hero[] = [];
+  title = 'War of Heroes';
+  selectedHero: Hero;
+  apiUrl = 'http://www.mocky.io/v2/596d86370f000059022b7de8'
+
+  constructor(private http: Http){
+    this.getHeroes();
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json())
+  }
+
+  getHeroes(){
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.heroes = data;
+    })
+  }
+
+  onSelect(hero: Hero): void {
+    console.log(hero);
+    this.selectedHero = hero;
+  }
 }
